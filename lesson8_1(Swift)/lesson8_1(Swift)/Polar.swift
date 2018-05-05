3b50a5a399e2b133bcd63c2b39aa282821d5face
Yura <yurzhuk@icloud.com>
2018-05-05 14:33:36 +0300

    initial commit

---
+++ lesson8_1(Swift)/lesson8_1(Swift)/Polar.swift
@@ -0,0 +1,20 @@
+//
+//  Polar.swift
+//  lesson8_1(Swift)
+//
+//  Created by yurii zhuk on 5/5/18.
+//  Copyright © 2018 yurii zhuk. All rights reserved.
+//
+
+import Foundation
+
+struct Polar {
+    var radius: Double
+    var angle: Int //{
+//        get {
+//            return self.angle
+//        } set {
+//            self.angle = Int(Double( newValue * 180) / Double.pi)
+//        }
+//    }
+}
